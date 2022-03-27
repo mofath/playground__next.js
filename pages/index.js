c7e1ba2dafa4d0fd03f3c14d195080e51635832b
mofath <mohamed.elgaedi@gmail.com>
2022-03-27 16:48:15 +0200

    added toolbar component to home and profile page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
-import styles from '../styles/Home.module.css'
+import { Toolbar } from '../components/toolbar';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Toolbar />
       Hello
     </div>
-  )
+  );
 }
